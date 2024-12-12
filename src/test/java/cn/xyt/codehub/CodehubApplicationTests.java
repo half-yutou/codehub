de083043fa1fed80c71fc935571cd31a6c92ea1b
御桐 徐 <8208220106@csu.edu.cn>
2024-12-12 23:04:05 +0800

    项目配置初始化

---
+++ src/test/java/cn/xyt/codehub/CodehubApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xyt.codehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodehubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
